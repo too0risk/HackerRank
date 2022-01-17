f323bdc3af57f3b357efef8d678452e09306708f
wahyupriambodo <wahyupriambodo.net@gmail.com>
2022-01-17 15:50:49 +0700

    Add submission in [C++] (Basic Data Types).

---
+++ cpp/basic-data-types/sol.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+#define nl "\n"
+
+int main() {
+    // Complete the code.
+    int i; long l; char c; float f; double d;
+    cin>>i>>l>>c>>f>>d;
+    printf("%d\n%ld\n%c\n%.3f\n%.9lf",i,l,c,f,d);
+    return 0;
+}
