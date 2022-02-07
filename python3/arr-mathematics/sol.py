180a6faad3ae526bc714efd6637103163d8f1558
wahyupriambodo <wahyupriambodo.net@gmail.com>
2022-02-07 13:32:06 +0700

    add array-mathematics solution.

---
+++ python3/arr-mathematics/sol.py
@@ -0,0 +1,14 @@
+import numpy
+
+if __name__=="__main__":
+	N,M=map(int, input().split())
+
+	A=numpy.array([list(map(int, input().split())) for n in range(N)])
+	B=numpy.array([list(map(int, input().split())) for n in range(N)])
+
+	print(A + B)
+	print(A - B)
+	print(A * B)
+	print(A // B)
+	print(A % B)
+	print(A ** B)
